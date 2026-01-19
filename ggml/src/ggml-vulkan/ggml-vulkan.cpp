8745ac4a84abb8eb6f6207d7e83deebd2f8d737b
Thomas Guillem <thomas@gllm.fr>
2026-01-19 12:59:45 +0100

    vulkan: don't throw an exception when init fails
    Print error and fallback to CPU instead.

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -668,6 +668,8 @@ struct vk_device_struct {
     std::mutex compile_mutex;
     std::condition_variable compile_cv;
 
+    bool initialization_failed = false;
+
     vk::PhysicalDevice physical_device;
     vk::PhysicalDeviceProperties properties;
     std::string name;
@@ -5764,8 +5766,9 @@ static vk_device ggml_vk_get_device(size_t idx) {
         std::vector<vk::PhysicalDevice> physical_devices = vk_instance.instance.enumeratePhysicalDevices();
 
         if (dev_num >= physical_devices.size()) {
-            std::cerr << "ggml_vulkan: Device with index " << dev_num << " does not exist." << std::endl;
-            throw std::runtime_error("Device not found");
+            device->initialization_failed = true;
+            GGML_LOG_ERROR("Device with index %zu does not exist", dev_num);
+            return nullptr;
         }
 
         device->physical_device = physical_devices[dev_num];
@@ -6632,7 +6635,11 @@ static vk_device ggml_vk_get_device(size_t idx) {
         return device;
     }
 
-    return vk_instance.devices[idx];
+    vk_device device = vk_instance.devices[idx];
+    if (device->initialization_failed) {
+        return nullptr;
+    }
+    return device;
 }
 
 static void ggml_vk_print_gpu_info(size_t idx) {
@@ -7115,6 +7122,9 @@ static void ggml_vk_init(ggml_backend_vk_context * ctx, size_t idx) {
     ctx->name = GGML_VK_NAME + std::to_string(idx);
 
     ctx->device = ggml_vk_get_device(idx);
+    if (!ctx->device) {
+        return;
+    }
 
     ctx->semaphore_idx = 0;
     ctx->event_idx = 0;
@@ -15382,6 +15392,9 @@ ggml_backend_buffer_type_t ggml_backend_vk_buffer_type(size_t dev_num) {
     VK_LOG_DEBUG("ggml_backend_vk_buffer_type(" << dev_num << ")");
 
     vk_device dev = ggml_vk_get_device(dev_num);
+    if (!dev) {
+        return nullptr;
+    }
 
     return &dev->buffer_type;
 }
@@ -15451,7 +15464,10 @@ ggml_backend_buffer_type_t ggml_backend_vk_host_buffer_type() {
 
     // Make sure device 0 is initialized
     ggml_vk_instance_init();
-    ggml_vk_get_device(0);
+    vk_device dev = ggml_vk_get_device(0);
+    if (!dev) {
+        return nullptr;
+    }
 
     return &ggml_backend_vk_buffer_type_host;
 }
@@ -17027,6 +17043,11 @@ ggml_backend_t ggml_backend_vk_init(size_t dev_num) {
 
     ggml_backend_vk_context * ctx = new ggml_backend_vk_context;
     ggml_vk_init(ctx, dev_num);
+
+    if (!ctx->device) {
+        delete ctx;
+        return nullptr;
+    }
 
     ggml_backend_t vk_backend = new ggml_backend {
         /* .guid    = */ ggml_backend_vk_guid(),
